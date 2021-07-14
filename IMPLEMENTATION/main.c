a6b08348d64381b89ebdc31a91c49847f9eb4467
lingawar-20 <srushtilingawar@gmail.com>
2021-07-14 21:00:47 +0530

    changes

--- IMPLEMENTATION/main.c
+++ IMPLEMENTATION/main.c
@@ -1,17 +1,19 @@
 
 #include"fun.h"
 #include<stdio.h>
-#include<conio.h>
+
 #include<stdlib.h>
 #include<windows.h>
 #include<string.h>
 int main()
 {
-    gotoxy(15,8);
+    
     printf("<--:employee_record_system:-->");
-    gotoxy(19,15);
+    char a,b;
+    scanf("%c",&a);
     printf("Press any key to continue.");
-    getch();
+    scanf("%c",&b);
+    
     menu();
     return 0;
 }
